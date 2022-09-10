8e7aaf76d8398e380deabd7ee1b05d5dec96d67d
OhashiReishi <ee207046@meiji.ac.jp>
2022-09-11 04:26:24 +0900

    Helloの表示の確認

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,13 @@ const app = express();
 
 app.use(express.raw({ type: "application/x-www-form-urlencoded" }));
 
+
+app.get("/list", (req: express.Request, res: express.Response) => {
+  res.send("Hello!");
+})
+
+
+
 // ルートにGETが来たらトップページへリダイレクト
 app.get("/", (req: express.Request, res: express.Response) => {
   console.log("/");
