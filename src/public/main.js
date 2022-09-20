98bb2f7fbe4d04ad21a25783acd683b40e0b7de7
soshi1429 <ee207107@meiji.ac.jp>
2022-09-20 14:28:44 +0900

    少し動きをつける

--- src/public/main.js
+++ src/public/main.js
@@ -16,3 +16,13 @@ pin.addEventListener(
   false
 );
 */
+
+window.onload = function () {
+  setTimeout(() => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loaded");
+
+    const content = document.querySelector(".content");
+    content.style.visibility = "visible";
+  }, 1300);
+};
