0e552abd836de0c8f0c96ae8135ef259125d0574
FumitoPIwata <ee207028@meiji.ac.jp>
2022-09-23 00:18:17 +0900

    createメソッドの引数を変更

--- src/db/init/init-members.ts
+++ src/db/init/init-members.ts
@@ -8,13 +8,13 @@ import memberModel from "../models/member-db";
 MemberDataSource.initialize()
   .then(async () => {
     // データの登録
-    await memberModel.create("akito", "0000");
-    await memberModel.create("sousi", "0001");
-    await memberModel.create("fumito", "0002");
-    await memberModel.create("reishi", "0003");
-    await memberModel.create("ryo", "0004");
-    await memberModel.create("takeyama", "0005");
-    await memberModel.create("takahiro", "0006");
-    await memberModel.create("moti", "0007");
+    await memberModel.create(6, "akito", "0000");
+    await memberModel.create(6, "sousi", "0001");
+    await memberModel.create(6, "fumito", "0002");
+    await memberModel.create(6, "reishi", "0003");
+    await memberModel.create(6, "ryo", "0004");
+    await memberModel.create(6, "takeyama", "0005");
+    await memberModel.create(6, "takahiro", "0006");
+    await memberModel.create(6, "moti", "0007");
   })
   .catch((error) => console.log(error));
