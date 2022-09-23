01fa859877c8a72ad59141059b56f2ac2a3349e5
Mochiyu <ee207057@meiji.ac.jp>
2022-09-23 20:50:49 +0900

    Jestのしょぼしょぼ実装

--- src/assign.test.ts
+++ src/assign.test.ts
@@ -1,37 +1,57 @@
-import { MemberDataSource } from "./db/sources/data-source";
-import {registerMember} from "./db/init/init-members";
 import certificator from "./controller/records/modules/certification";
-import express from "express";
+import memberModel from "./db/models/member-db";
+import typeorm from "./db/init/init-orm";
 
-type TestType = {
-    name:string;
-    pin:string;
-}
+beforeEach(async () => {});
 
-const testArray:Array<TestType> =[
-    {name:"test1",pin:"0000"},
-    {name:"A",pin:"0001"},
-    {name:"TEST3",pin:"0002"},
-    {name:"samepin1",pin:"0003"},
-    {name:"samepin2",pin:"0003"}
-];
+afterAll(() => {
+  console.log("Test is over");
+});
 
-const app = express();
-app.use(express.static("./dist/public"));
-
-
-describe("#1 nameとpinが真",()=>{
-
-        MemberDataSource.initialize().then(async()=>
-            testArray.forEach(async (value,index,array)=>{
-                console.log("adding element");
-                await registerMember(value.name,value.pin)
-            })
-        ).catch((error) => console.log(error));
-
-    testArray.forEach((value,index,array)=>{
-        test("test:" + index,async ()=>{
-            expect(await certificator.certificate(value.name,value.pin)).toBe({ cert: true, message: "" });
-        });
-    })
+describe("#1 nameとpinが真", () => {
+  test("Jestのテスト", async () => {
+    await typeorm.initMember(async () => {
+      // データの登録
+      await memberModel.create(6, "akito", "0000");
+      await memberModel.create(6, "sousi", "0001");
+      await memberModel.create(6, "fumito", "0002");
+      await memberModel.create(6, "reishi", "0003");
+      await memberModel.create(6, "ryo", "0004");
+      await memberModel.create(6, "takeyama", "0005");
+      await memberModel.create(6, "takahiro", "0006");
+      await memberModel.create(6, "moti", "0007");
+      await expect(await certificator.certificate("akito", "0000")).toBe({
+        cert: true,
+        message: "",
+      });
+      await expect(await certificator.certificate("sousi", "0001")).toBe({
+        cert: true,
+        message: "",
+      });
+      await expect(await certificator.certificate("fumito", "0002")).toBe({
+        cert: true,
+        message: "",
+      });
+      await expect(await certificator.certificate("reishi", "0003")).toBe({
+        cert: true,
+        message: "",
+      });
+      await expect(await certificator.certificate("ryo", "0004")).toBe({
+        cert: true,
+        message: "",
+      });
+      await expect(await certificator.certificate("takeyama", "0005")).toBe({
+        cert: true,
+        message: "",
+      });
+      await expect(await certificator.certificate("takahiro", "0006")).toBe({
+        cert: true,
+        message: "",
+      });
+      await expect(await certificator.certificate("moti", "0007")).toBe({
+        cert: true,
+        message: "",
+      });
+    });
+  });
 });
