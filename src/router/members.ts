12675de2b33fc9a05af1cdef1000e657f939a098
FumitoPIwata <ee207028@meiji.ac.jp>
2022-09-24 17:14:16 +0900

    フロントから送られたpasswordをハッシュ化

--- src/router/members.ts
+++ src/router/members.ts
@@ -1,5 +1,8 @@
 import express from "express";
+
+import memberModel from "../db/models/member-db";
 import register from "./members/register";
+import sha256 from "../controller/records/modules/sha256";
 
 const router = express.Router();
 
@@ -7,9 +10,19 @@ const router = express.Router();
 router.use("/register", register);
 
 router.route("/").get(async (req: express.Request, res: express.Response) => {
-  const body = req.body;
-  
   res.redirect("/members/register");
+}).post(async (req: express.Request, res: express.Response) => {
+  const body = req.body;
+
+  const grade = body.grade;
+  const name = body.name;
+  const pass = body.password; //　body.passwordはfrontでもハッシュ化されたもの
+  const hashedpass = sha256(pass);
+
+await memberModel.create(grade, name, hashedpass);
+
+
+  res.redirect("/inout");
 });
 
 export default router;
