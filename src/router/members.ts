bab4cbb480b1a138e4150ba421c7e84ed483b856
FumitoPIwata <ee207028@meiji.ac.jp>
2022-09-24 17:24:15 +0900

    buildによるコード整形とエラー解消

--- src/router/members.ts
+++ src/router/members.ts
@@ -9,20 +9,21 @@ const router = express.Router();
 // /memberに来たリクエストの処理
 router.use("/register", register);
 
-router.route("/").get(async (req: express.Request, res: express.Response) => {
-  res.redirect("/members/register");
-}).post(async (req: express.Request, res: express.Response) => {
-  const body = req.body;
+router
+  .route("/")
+  .get(async (req: express.Request, res: express.Response) => {
+    res.redirect("/members/register");
+  })
+  .post(async (req: express.Request, res: express.Response) => {
+    const body = req.body;
 
-  const grade = body.grade;
-  const name = body.name;
-  const pass = body.password; //　body.passwordはfrontでもハッシュ化されたもの
-  const hashedpass = sha256(pass);
+    const grade = body.grade;
+    const name = body.name;
+    const pass = body.password; // body.passwordはfrontでもハッシュ化されたもの
+    const hashedpass = sha256(pass);
+    await memberModel.create(grade, name, hashedpass);
 
-await memberModel.create(grade, name, hashedpass);
-
-
-  res.redirect("/inout");
-});
+    res.redirect("/inout");
+  });
 
 export default router;
