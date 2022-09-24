8bced0ed6df29130462f5a077bbe05b291a29c8c
OhashiReishi <ee207046@meiji.ac.jp>
2022-09-24 15:39:54 +0900

    メソッドの戻り値の型指定

--- src/controller/records/modules/sha256.ts
+++ src/controller/records/modules/sha256.ts
@@ -1,7 +1,7 @@
 import crypto from "crypto";
 
-const sha256 = (text: string) => {
-    return crypto.createHash("sha256").update(text).digest("hex");
-}
+const sha256 = (text: string): string => {
+  return crypto.createHash("sha256").update(text).digest("hex");
+};
 
 export default sha256;
