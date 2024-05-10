501bfe7fd054904aecdfee97fe7ef10e89bbb4dd
paulabear648 <paulabear648@gmail.com>
2024-05-10 12:31:07 +0900

    データベース初期化時に登録する人の名前を変更

--- db/init/init-members.ts
+++ db/init/init-members.ts
@@ -9,26 +9,26 @@ import recordModel from "../models/record-db";
   
   // データの登録
   // ゼミ長・副ゼミ長を登録しております
-  await memberModel.create(4, "阿部", "0000");
-  await memberModel.create(4, "平山", "0001");
-  await memberModel.create(5, "杉田", "0002");
-  await memberModel.create(5, "松本", "0003");
-  await memberModel.create(6, "竹山", "0004");
-  await memberModel.create(6, "大橋", "0005");
+  await memberModel.create(4, "田中", "0000");
+  await memberModel.create(4, "大山", "0001");
+  await memberModel.create(5, "佐藤", "0002");
+  await memberModel.create(5, "山田", "0003");
+  await memberModel.create(6, "鈴木", "0004");
+  await memberModel.create(6, "岩田", "0005");
 
   // ログの登録(工数の都合で正規化されていません！)
-  await recordModel.create("阿部", "入室");
-  await recordModel.create("平山", "入室");
-  await recordModel.create("平山", "退室");
-  await recordModel.create("大橋", "入室");
-  await recordModel.create("杉田", "入室");
-  await recordModel.create("松本", "入室");
-  await recordModel.create("阿部", "退室");
-  await recordModel.create("松本", "退室");
-  await recordModel.create("杉田", "退室");
-  await recordModel.create("大橋", "退室");
-  await recordModel.create("竹山", "入室");
-  await recordModel.create("竹山", "退室");  
+  await recordModel.create("田中", "入室");
+  await recordModel.create("大山", "入室");
+  await recordModel.create("大山", "退室");
+  await recordModel.create("岩田", "入室");
+  await recordModel.create("佐藤", "入室");
+  await recordModel.create("山田", "入室");
+  await recordModel.create("田中", "退室");
+  await recordModel.create("山田", "退室");
+  await recordModel.create("佐藤", "退室");
+  await recordModel.create("岩田", "退室");
+  await recordModel.create("鈴木", "入室");
+  await recordModel.create("鈴木", "退室");  
 })()
   .then()
   .catch((error) => console.log(error));
